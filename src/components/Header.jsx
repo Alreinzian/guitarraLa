7de0232790900ed65adc23585253325067d99351
Daniel Manrique <sagender1623@gmail.com>
2025-01-09 19:35:59 -0500

    Fragment componentes

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+//import {Fragment} from 'react'
+//import React fron 'react'
+
+export default function Header(){
+
+    const total = 100
+
+    return (
+       <>
+        <p>Total a pagar: {total}</p>
+        <p>Después de pagar</p>
+      </>
+
+       //<div>
+           // <p>Total a pagar: {total}</p>
+        //</div>
+        //<Fragment>
+          
+        //<p>Total a pagar: {total}</p>
+          //  <p>Después de pagar</p>
+        //</Fragment>
+
+      
+    )
+}
+
